123a83caa86998dfa652ec1ec45cf959dbd00557
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-05-29 16:47:52 -0700

    add pages without Redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import Contact from './components/Contact';
+import Navbar from './components/Navbar';
+import Post from './components/Post';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header' />
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/contact' component={Contact} />
+          <Route path='/:post_id' component={Post} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/hoc/Rainbow.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Rainbow = WrapperComponent => {
+  const colours = ['red', 'pink', 'orange', 'blue', 'green', 'yellow'];
+  const randomColour = colours[Math.floor(Math.random() * 5)];
+  const className = randomColour + '-text';
+
+  return props => {
+    return (
+      <div className={className}>
+        <WrapperComponent props={props} />
+      </div>
+    );
+  };
+};
+
+export default Rainbow;
